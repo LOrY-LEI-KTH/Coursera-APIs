d4d3c6e018901c7bffc1ae691df30132910bac07
Yuchen Lei <yuchenl@kth.se>
2025-02-19 21:03:41 +0100

    Reapply "Merge branch 'master' of https://github.com/LOrY-LEI-KTH/Coursera-APIs"
    This reverts commit 3c1f9685789a2f74af7a1097df94143ab3f1a66a.

---
+++ Little Lemom API project/LittleLemon/LittleLemonAPI/views.py
@@ -0,0 +1,15 @@
+from django.shortcuts import render
+from rest_framework.decorators import api_view, permission_classes
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.response import Response
+
+# Create your views here.
+
+
+@api_view()
+@permission_classes([IsAuthenticated])
+def manager_view(request):
+    if request.user.groups.filter(name='Manager').exists():
+        return Response({"message":"Only Manager should see this"})
+    else 
+        return Response({"message":"You are not authorized"},403)
